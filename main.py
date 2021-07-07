549ad0c697daa4f70431c77a3bc7ae954d6a754f
clout <59515681+cloutjs@users.noreply.github.com>
2021-07-07 21:22:21 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,6 @@ options.add_argument('--headless')
 browser = webdriver.Chrome("chromedriver.exe", options=options)
 content = browser.page_source
 
-
 with open('names.txt') as e:
     usernames = e.read().splitlines()
     for name in usernames:
@@ -39,5 +38,4 @@ with open('names.txt') as e:
         except:
             print(f"{Fore.RED}[!] You are rate limited")
             system(f"title Uplay Name Checker by clout // Checked: {count}/{len(usernames)} - Hits: {hits}")
-
 print(f"{Fore.WHITE}\n\nDone Checking")
